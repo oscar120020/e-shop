d69b154769a2d5ed221ef4822add7b864e8624eb
oscar120020 <oscarmiguel12002@gmail.com>
2025-07-29 12:59:06 -0400

    add provider google

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -24,10 +24,10 @@ export const authOptions: NextAuthOptions = {
     //   clientId: process.env.GITHUB_ID || "",
     //   clientSecret: process.env.GITHUB_SECRET || "",
     // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID || "",
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-    // })
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    })
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET,
